47cadf40bce66800122e28af3fb2672e4fb6416c
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2021-11-21 10:47:10 -0800

    Added swap info functions to mem_info
    Functions to retrieve info related to swap mem usage have been added to mem_info.py

--- mem_info.py
+++ mem_info.py
@@ -21,4 +21,20 @@ def get_mem_free():
     return psutil.virtual_memory().free
 
 
+def get_swap_total():
+    return psutil.swap_memory().total
+
+
+def get_swap_used():
+    return psutil.swap_memory().used
+
+
+def get_swap_free():
+    return psutil.swap_memory().free
+
+
+def get_swap_used_percent():
+    return round((get_swap_used() / get_swap_total()) * 100, 2)
+
+
 print(get_total_mem())
