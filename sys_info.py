6b26d3f7ae8c8dff3004bd85dcd62232d52fdad5
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2021-11-24 09:36:17 -0800

    Added function to retrieve battery and super username

--- sys_info.py
+++ sys_info.py
@@ -19,3 +19,11 @@ def get_version():
 
 def get_startup_time():
     return datetime.fromtimestamp(psutil.boot_time())
+
+
+def get_battery():
+    return psutil.sensors_battery()
+
+
+def get_super_usr():
+    return psutil.users()[0].name
