2a912f6514ec6e6ac46d4b7e32cc7bc364673f49
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2021-11-25 20:33:16 -0800

    Renamed label to value

--- cpu_gui.py
+++ cpu_gui.py
@@ -5,7 +5,7 @@ import cpu_stats
 class CPUGui:
     cpu_frame = None
     master = None
-    cpu_usage_label = None
+    cpu_usage_val = None
 
     def __init__(self, master):
         self.master = master
@@ -41,28 +41,28 @@ class CPUGui:
         logic_core_title.grid(sticky="W", column=0, row=4, pady=(0, 2), padx=(4, 10))
 
     def init_cpu_vals(self):
-        cpu_name_label = Label(self.cpu_frame, background="white", fg="DodgerBlue3", text=cpu_stats.get_processor(),
-                               font="Arial 12", anchor="e")
-        cpu_name_label.grid(sticky="E", column=1, row=0, pady=(0, 2), padx=(174, 4))
+        cpu_name_val = Label(self.cpu_frame, background="white", fg="DodgerBlue3", text=cpu_stats.get_processor(),
+                             font="Arial 12", anchor="e")
+        cpu_name_val.grid(sticky="E", column=1, row=0, pady=(0, 2), padx=(174, 4))
 
-        self.cpu_usage_label = Label(self.cpu_frame, background="white", fg="DodgerBlue3",
-                                     text=str(cpu_stats.get_total_cpu_usage()) + "%", font="Arial 12", anchor="e")
-        self.cpu_usage_label.grid(sticky="E", column=1, row=1, pady=(0, 2), padx=(174, 4))
+        self.cpu_usage_val = Label(self.cpu_frame, background="white", fg="DodgerBlue3",
+                                   text=str(cpu_stats.get_total_cpu_usage()) + "%", font="Arial 12", anchor="e")
+        self.cpu_usage_val.grid(sticky="E", column=1, row=1, pady=(0, 2), padx=(174, 4))
 
-        cpu_freq_label = Label(self.cpu_frame, background="white", fg="DodgerBlue3", text=cpu_stats.get_total_cpu_freq(),
-                               font="Arial 12", anchor="e")
-        cpu_freq_label.grid(sticky="E", column=1, row=2, pady=(0, 2), padx=(174, 4))
+        cpu_freq_val = Label(self.cpu_frame, background="white", fg="DodgerBlue3", text=cpu_stats.get_total_cpu_freq(),
+                             font="Arial 12", anchor="e")
+        cpu_freq_val.grid(sticky="E", column=1, row=2, pady=(0, 2), padx=(174, 4))
 
-        cpu_phys_label = Label(self.cpu_frame, background="white", fg="DodgerBlue3", text=cpu_stats.get_phys_core_count(),
-                               font="Arial 12", anchor="e")
-        cpu_phys_label.grid(sticky="E", column=1, row=3, pady=(0, 2), padx=(174, 4))
+        cpu_phys_val = Label(self.cpu_frame, background="white", fg="DodgerBlue3", text=cpu_stats.get_phys_core_count(),
+                             font="Arial 12", anchor="e")
+        cpu_phys_val.grid(sticky="E", column=1, row=3, pady=(0, 2), padx=(174, 4))
 
-        cpu_logic_label = Label(self.cpu_frame, background="white", fg="DodgerBlue3",
-                                text=cpu_stats.get_logical_core_count(), font="Arial 12", anchor="e")
-        cpu_logic_label.grid(sticky="E", column=1, row=4, pady=(0, 2), padx=(174, 4))
+        cpu_logic_val = Label(self.cpu_frame, background="white", fg="DodgerBlue3",
+                              text=cpu_stats.get_logical_core_count(), font="Arial 12", anchor="e")
+        cpu_logic_val.grid(sticky="E", column=1, row=4, pady=(0, 2), padx=(174, 4))
 
     def update_cpu_usage(self):
-        self.cpu_usage_label.configure(text=str(cpu_stats.get_total_cpu_usage()) + "%")
+        self.cpu_usage_val.configure(text=str(cpu_stats.get_total_cpu_usage()) + "%")
 
     def cpu_updater(self):
         self.update_cpu_usage()
